a46e05d07d0ade08968f9c0e56f0ef1513959581
chinbor <chinbor.mn@gmail.com>
2023-03-29 20:13:01 +0800

    feat: update type for component

--- volar.d.ts
+++ volar.d.ts
@@ -20,7 +20,6 @@ declare module 'vue' {
     OTag: typeof import('onu-ui')['OTag']
     OTrigger: typeof import('onu-ui')['OTrigger']
     OBacktop: typeof import('onu-ui')['OBacktop']
-    OVirtualList: typeof import('onu-ui')['OVirtualList']
   }
 }
 export {}
